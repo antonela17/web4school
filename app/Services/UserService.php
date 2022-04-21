157561aabda57812401c8936c36ae4e5ff215849
antonela17 <antonelakolaj17@gmail.com>
2022-04-21 19:20:13 +0200

    CRUD: Creating first classes

---
+++ app/Services/UserService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+
+    public static function createData(Request $request)
+    {
+        return User::create([
+            'name' => $request->input('name'),
+            'surname' => $request->input('surname'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'role_id'=> $request->input('role'),
+            'class_id'=>null,
+        ]);
+    }
+}
