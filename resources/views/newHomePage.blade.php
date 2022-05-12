248cf8378f08e06231445ca1cd7f5c763b351581
amiZeqiri <amina.zeqiri@fshnstudent.info>
2022-05-12 20:15:57 +0200

    HomePage Design

---
+++ resources/views/newHomePage.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+ 
+<html>
+ 
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+    <link href="{{ asset('css/newHomePage.css') }}" rel="stylesheet">
+    
+
+</head>
+ 
+<body>
+    <nav class="navbar background">
+        <ul class="nav-list">
+            <div class="logo">
+                <img src= "logo.png">
+            </div>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#newLogin">LogIn</a></li>
+            <li><a href="#newRegister">Register</a></li>
+        </ul>
+ 
+        <div class="rightNav">
+            <input type="text" name="search" id="search">
+            <button class="btn btn-sm">Search</button>
+        </div>
+    </nav>
+    <section class="firstsection">
+        
+    </section>
+    <footer class="background">
+        <p class="text-footer">
+            Copyright ©-All rights are reserved
+        </p>
+    </footer>
+</body>
+ 
+</html>
