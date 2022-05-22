d67827565557d965bd938e497592cd5d5d24d2f2
antonela17 <antonelakolaj17@gmail.com>
2022-05-22 16:29:34 +0200

    Change-Design-Ela: Showing files.

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Response;
+
+class FileController extends Controller
+{
+    public function index(){
+        return view('file.index');
+    }
+    public function showFiles($id){
+        $files=['files/test.pdf','files/vertetimStudenti.pdf'];
+        return view('file.showFiles')->with(compact('files'));
+    }
+    public function getDocument(Request $request)
+    {
+if ($request['filename']){
+        $filePath = $request['filename'];
+
+        // file not found
+        if( ! Storage::disk('local')->exists($filePath) ) {
+            abort(404);
+        }
+
+        $pdfContent = Storage::disk('local')->get($filePath);
+
+        // for pdf, it will be 'application/pdf'
+        $type       = Storage::disk('local')->mimeType($filePath);
+        $fileName   = 'test';
+
+        return Storage::disk('local')->response($filePath);
+    }
+return redirect()->back();
+}
+}
