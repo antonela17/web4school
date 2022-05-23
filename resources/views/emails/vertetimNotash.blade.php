eee22c0a300301319f53d5f1985053b0c60cd14a
antonela17 <antonelakolaj17@gmail.com>
2022-05-23 23:46:42 +0200

    Change-Design-Ela: Vertetim Notash view

---
+++ resources/views/emails/vertetimNotash.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE>
+<html>
+<style>
+    body{
+
+        margin: 0;
+
+        background-color: bisque;
+
+    }
+
+    .index {
+
+        align-items: center;
+
+        justify-content: center;
+
+    }
+
+    .index .top img{
+
+        justify-content: center;
+
+        width: 65px;
+
+        display: block;
+
+        margin-left: auto;
+
+        margin-right: auto;
+
+        margin-top: 61px;
+
+    }
+
+    .index .top h2{
+
+        justify-content: center;
+
+        align-items: center;
+
+        text-align: center;
+
+    }
+    .index .bottom p{
+
+        font-size: 17px;
+
+        margin-top: 70px;
+
+    }
+    .bottom{
+
+        justify-content: center;
+
+        align-items: center;
+
+        text-align: center;
+
+        margin-left: 60px;
+
+        margin-right: 60px;
+
+    }
+
+</style>
+<title>Document</title>
+<body>
+<div class="index">
+    <div class="top">
+        <h2 >
+            REPUBLIKA E SHQIPERISE <br>
+            SHKOLLA WEB4SCHOOL <br>
+            DEGA MESIMORE<br>
+        </h2>
+    </div>
+    <div class="bottom">
+        <h3>VERTETIM</h3>
+        <p> Vertetohet se <b>Z. Znj Aida Kolaj</b>, vazhdon Ciklin e Pare te Studimeve "Bachelor< ne "Informatike/ , sistemi me kohe
+            te plote , viti i lll-te i rregullt per vitin akademik 2021-2022 ne baze te Regjistrimit themeltar
+            te notave te Deges Mesimore te Fakultetit te Shkencave te Natyres, rezulton se ka shlyer keto detyrime me vleresimet perkatese:
+        </p>
+    </div>
+    <div class="row">
+        <div class="column" >
+            <h3>NR</h3>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+
+        </div>
+        <div class="column" >
+            <h3>LENDA</h3>
+            <p>Fizike</p>
+            <p>Matematike</p>
+            <p>Algjeber</p>
+            <p>Gjeografi</p>
+            <p>Histori</p>
+
+
+        </div>
+        <div class="column">
+            <h3>NOTAT</h3>
+            <p>9</p>
+            <p>6</p>
+            <p>8</p>
+            <p>10</p>
+            <p>7</p>
+
+        </div>
+        <p style=" font-weight:bold; right:0 !important;">Mesatarja : 8.9</p>
+
+    </div>
+</div>
+</body>
+</html>
